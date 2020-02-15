28b38d2d0be3e5555bd57340a502913a9c23541a
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-15 11:52:46 +0300

    Fix parsing tools, now it reqire only copy-paste from source, set up theme and type id

--- sql/tools/EditionsParser/Main.java
+++ sql/tools/EditionsParser/Main.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -16,22 +18,51 @@ public class Main {
         String format = "INSERT INTO edition(name, type, theme_id, periodicity_per_year, minimum_subscription_period_in_months, price_for_minimum_subscription_period) VALUES(\"%s\", \"%s\", %d, %d, %d, %.2f);";
 
 	    List<String> list = Files.readAllLines(Paths.get("input.txt"));
+	    String allText = new String();
+	    for (String s : list) {
+	        allText += s + "\n";
+        }
+	    String regex1 = "(?s)\\n\\n";
+	    String replace1 = "";
+        String regex2 = "(?s)\\t";
+        String replace2 = "";
+        //String regex3 = "(?s)\\n(.+)\\n(.+)\\n(.+)\\n(.+)";
+        //String replace3 = "$1;$2;$3;$4\\n";
+        String regex4 = "(?s)\\d{5}\\b";
+        String replace4 = "";
+        String regex5 = "(?s)(\\n\\d+),";
+        String replace5 = "$1.";
+/*
+        allText = Pattern.compile(regex1, Pattern.DOTALL).matcher(allText).replaceAll(replace1);
+        allText = Pattern.compile(regex2, Pattern.DOTALL).matcher(allText).replaceAll(replace2);
+        allText = Pattern.compile(regex3, Pattern.DOTALL).matcher(allText).replaceAll(replace3);
+        allText = Pattern.compile(regex4, Pattern.DOTALL).matcher(allText).replaceAll(replace4);
+        allText = Pattern.compile(regex5, Pattern.DOTALL).matcher(allText).replaceAll(replace5);
+
+ */
+        allText = allText.replaceAll(regex1, replace1);
+        allText = allText.replaceAll(regex2, replace2);
+        //allText = allText.replaceAll(regex3, replace3);
+        allText = allText.replaceAll(regex4, replace4);
+        allText = allText.replaceAll(regex5, replace5);
+        String[] allArray = allText.split("\n");
 	    Files.write(Paths.get("output.txt"), "".getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-	    for (String string : list) {
-	        String[] array = string.split(";");
+	    for (int i = 1; i < allArray.length;) {
+
             try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt", true))) {
                 bufferedWriter.write(String.format(format,
-                        array[0].replaceAll("\"", "\\\\\""),
-                        "Газета",
-                        5,
-                        new Scanner(array[1]).nextInt() * 2,
-                        new Scanner(array[2]).nextInt(),
-                        new Scanner(array[3]).nextDouble()
+                        allArray[i++].replaceAll("\"", "\\\\\""),
+                        "Журнал",
+                        24,
+                        new Scanner(allArray[i++]).nextInt() * 2,
+                        new Scanner(allArray[i++]).nextInt(),
+                        new Scanner(allArray[i++]).nextDouble()
                 ));
                 bufferedWriter.write(System.lineSeparator());
 
-            } catch (IOException e) {
+            } catch (IOException | InputMismatchException e) {
                 e.printStackTrace();
+                //System.out.println("string = " + string);
             }
         }
     }
