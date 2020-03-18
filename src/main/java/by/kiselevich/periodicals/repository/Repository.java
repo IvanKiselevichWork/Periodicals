0f6dcdf0a2902a07f9683b71195d7e39490292a8
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-03-18 10:55:51 +0300

    Add throwing RepositoryException to Repository methods

--- src/main/java/by/kiselevich/periodicals/repository/Repository.java
+++ src/main/java/by/kiselevich/periodicals/repository/Repository.java
@@ -1,16 +1,17 @@
 package by.kiselevich.periodicals.repository;
 
+import by.kiselevich.periodicals.exception.RepositoryException;
 import by.kiselevich.periodicals.specification.Specification;
 
 import java.util.List;
 
 public interface Repository<T> {
 
-    void add(T t);
+    void add(T t) throws RepositoryException;
 
-    void remove(T t);
+    void remove(int id) throws RepositoryException;
 
-    void update(T t);
+    void update(T t) throws RepositoryException;
 
-    List<T> query(Specification<T, Repository<T>> specification);
+    List<T> query(Specification<T, Repository<T>> specification) throws RepositoryException;
 }
