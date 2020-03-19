b849a540a4f48b6c399e7e1befd0fa4423c5eabb
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-03-19 10:10:09 +0300

    Add runtime exception for connection pool

---
+++ src/main/java/by/kiselevich/periodicals/exception/ConnectionPoolRuntimeException.java
@@ -0,0 +1,23 @@
+package by.kiselevich.periodicals.exception;
+
+public class ConnectionPoolRuntimeException extends RuntimeException {
+
+    public ConnectionPoolRuntimeException() {
+    }
+
+    public ConnectionPoolRuntimeException(String message) {
+        super(message);
+    }
+
+    public ConnectionPoolRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConnectionPoolRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public ConnectionPoolRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
