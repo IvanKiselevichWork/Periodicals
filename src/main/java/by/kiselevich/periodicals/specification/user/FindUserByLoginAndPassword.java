b9d243764e0e3a7a989693391536cdedf8ecf431
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-03-20 16:18:06 +0300

    Add getting users by login and password specification

--- src/main/java/by/kiselevich/periodicals/specification/user/FindUserByLoginAndPassword.java
+++ src/main/java/by/kiselevich/periodicals/specification/user/FindUserByLoginAndPassword.java
@@ -1,18 +1,47 @@
 package by.kiselevich.periodicals.specification.user;
 
 import by.kiselevich.periodicals.entity.User;
+import by.kiselevich.periodicals.exception.RepositoryException;
+import by.kiselevich.periodicals.pool.ConnectionPool;
+import by.kiselevich.periodicals.pool.ConnectionProxy;
 import by.kiselevich.periodicals.repository.Repository;
+import by.kiselevich.periodicals.util.HashUtil;
+import by.kiselevich.periodicals.util.RepositoryUtil;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindUserByLoginAndPassword implements UserSpecification {
 
+    private static final String FIND_USERS_BY_LOGIN_AND_PASSWORD = "select id, login, password, full_name, email, money, role_id, is_available from user where login = ? and password = ?";
+
+    private String login;
+    private String password;
+
     public FindUserByLoginAndPassword(String login, char[] password) {
-        //todo
+        this.login = login;
+        this.password = HashUtil.getHash(password, login);
+        Arrays.fill(password, '0');
     }
 
     @Override
-    public List<User> query(Repository<User> repository) {
-        return null; ///todo
+    public List<User> query(Repository<User> repository) throws RepositoryException {
+        ResultSet resultSet = null;
+        List<User> users;
+        try (ConnectionProxy connection = ConnectionPool.INSTANCE.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(FIND_USERS_BY_LOGIN_AND_PASSWORD);
+            statement.setString(1, login);
+            statement.setString(2, password);
+            resultSet = statement.executeQuery();
+            users = RepositoryUtil.getUsersFromResultSet(resultSet);
+        } catch (SQLException e) {
+            throw new RepositoryException(e);
+        } finally {
+            RepositoryUtil.closeResultSet(resultSet);
+        }
+        return users;
     }
 }
