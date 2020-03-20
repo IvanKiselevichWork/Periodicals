082b96ae73c700cb322c1693504c64c6efab4ee3
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-03-20 16:16:15 +0300

    Add method to repository util for getting users from result set

--- src/main/java/by/kiselevich/periodicals/util/RepositoryUtil.java
+++ src/main/java/by/kiselevich/periodicals/util/RepositoryUtil.java
@@ -1,10 +1,13 @@
 package by.kiselevich.periodicals.util;
 
+import by.kiselevich.periodicals.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RepositoryUtil {
 
@@ -20,5 +23,22 @@ public class RepositoryUtil {
                 LOG.warn(e);
             }
         }
+    }
+
+    public static List<User> getUsersFromResultSet(ResultSet resultSet) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while (resultSet.next()) {
+            users.add(new User(
+                    resultSet.getInt("id"),
+                    resultSet.getInt("role_id"),
+                    resultSet.getString("login"),
+                    resultSet.getString("password").toCharArray(),
+                    resultSet.getString("full_name"),
+                    resultSet.getString("email"),
+                    resultSet.getBigDecimal("money"),
+                    resultSet.getBoolean("is_available")
+            ));
+        }
+        return users;
     }
 }
