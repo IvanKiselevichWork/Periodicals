d404b0d2427497d08780c628452d2e560d426673
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-03-23 11:29:29 +0300

    Remove unnecessary tests

--- src/test/java/by/kiselevich/periodicals/pool/ConnectionPoolTest.java
+++ src/test/java/by/kiselevich/periodicals/pool/ConnectionPoolTest.java
@@ -2,8 +2,6 @@ package by.kiselevich.periodicals.pool;
 
 import by.kiselevich.periodicals.exception.NoJDBCDriverException;
 import by.kiselevich.periodicals.exception.NoJDBCPropertiesException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,8 +12,6 @@ import java.sql.SQLException;
 
 public class ConnectionPoolTest {
 
-    static final Logger LOG = LogManager.getLogger(ConnectionPoolTest.class);
-
     private ConnectionPool connectionPool;
 
     @Before
@@ -25,7 +21,7 @@ public class ConnectionPoolTest {
     }
 
     @Test
-    public void ConnectionPoolTest1() throws SQLException {
+    public void testConnectionPool() throws SQLException {
         try (Connection connection = connectionPool.getConnection()) {
             Assert.assertNotNull(connection);
         }
@@ -34,60 +30,5 @@ public class ConnectionPoolTest {
     @After
     public void deInit() {
         connectionPool.deInitPool();
-    }
-
-    //todo delete this test on release
-    //@Test
-    public void ConnectionPoolThreadsTest() {
-        int threadsNumber = 5;
-        LOG.trace("threads test started");
-        Thread[] threadsInit = new Thread[threadsNumber];
-        Thread[] threadsDeInit = new Thread[threadsNumber];
-        Thread thread;
-        for (int i = 0; i < threadsNumber; i++) {
-            thread = new PoolTesterInit();
-            thread.start();
-            threadsInit[i] = thread;
-
-            thread = new PoolTesterDeInit();
-            thread.start();
-            threadsDeInit[i] = thread;
-        }
-        try {
-            for (Thread thread1 : threadsInit) {
-                thread1.join();
-            }
-            for (Thread thread1 : threadsDeInit) {
-                thread1.join();
-            }
-        } catch (InterruptedException e) {
-            LOG.error(e);
-        }
-        ConnectionPool.INSTANCE.deInitPool();
-        LOG.trace("threads test ended");
-        Assert.assertTrue(true);
-    }
-}
-
-class PoolTesterInit extends Thread {
-
-    @Override
-    public void run() {
-        try {
-            ConnectionPoolTest.LOG.trace("init start : {}", getName());
-            ConnectionPool.INSTANCE.initPool();
-            ConnectionPoolTest.LOG.trace("init end : {}", getName());
-        } catch (NoJDBCPropertiesException | NoJDBCDriverException e) {
-            ConnectionPoolTest.LOG.error(e);
-        }
-    }
-}
-
-class PoolTesterDeInit extends Thread {
-    @Override
-    public void run() {
-        ConnectionPoolTest.LOG.trace("deInit start : {}", getName());
-        ConnectionPool.INSTANCE.deInitPool();
-        ConnectionPoolTest.LOG.trace("deInit end : {}", getName());
     }
 }
