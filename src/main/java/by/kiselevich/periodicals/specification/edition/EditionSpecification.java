6588ad119ffc9c336de8250ac346a2b36d14902d
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-03-25 22:14:39 +0300

    Add base of edition and payment specification

---
+++ src/main/java/by/kiselevich/periodicals/specification/edition/EditionSpecification.java
@@ -0,0 +1,7 @@
+package by.kiselevich.periodicals.specification.edition;
+
+import by.kiselevich.periodicals.entity.Edition;
+import by.kiselevich.periodicals.specification.Specification;
+
+public interface EditionSpecification extends Specification<Edition> {
+}
---
+++ src/main/java/by/kiselevich/periodicals/specification/payment/PaymentSpecification.java
@@ -0,0 +1,7 @@
+package by.kiselevich.periodicals.specification.payment;
+
+import by.kiselevich.periodicals.entity.Payment;
+import by.kiselevich.periodicals.specification.Specification;
+
+public interface PaymentSpecification extends Specification<Payment> {
+}
