19cd6f9b183b3bc97dce05d57ede43351c4a9423
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-03-26 15:42:02 +0300

    Add subscription repository interface and implementation (base)

---
+++ src/main/java/by/kiselevich/periodicals/repository/subscription/SubscriptionRepository.java
@@ -0,0 +1,7 @@
+package by.kiselevich.periodicals.repository.subscription;
+
+import by.kiselevich.periodicals.entity.Subscription;
+import by.kiselevich.periodicals.repository.Repository;
+
+public interface SubscriptionRepository extends Repository<Subscription> {
+}
