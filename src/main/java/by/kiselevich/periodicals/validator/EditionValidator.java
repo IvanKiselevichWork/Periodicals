702893f1eb2a08f063d78cb0df50780daf7b4220
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-03-31 09:27:25 +0300

    Fix edition validator, typeId and themeId check WIP

--- src/main/java/by/kiselevich/periodicals/validator/EditionValidator.java
+++ src/main/java/by/kiselevich/periodicals/validator/EditionValidator.java
@@ -4,17 +4,13 @@ import by.kiselevich.periodicals.command.ResourceBundleMessages;
 import by.kiselevich.periodicals.entity.Edition;
 import by.kiselevich.periodicals.exception.EditionValidatorException;
 
+import java.math.BigDecimal;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EditionValidator {
 
     private static final String NAME_REGEX = ".{1,200}";
-    private static final String TYPE_REGEX = ".{1,200}";
-    private static final String THEME_ID_REGEX = "\\d{1}";
-    private static final String PERIODICITY_PER_YEAR_REGEX = "\\d{1,3}";
-    private static final String MINIMUM_SUBSCRIPTION_PERIOD_IN_MONTHS_REGEX = "\\d{1,3}";
-    private static final String PRICE_FOR_MINIMUM_SUBSCRIPTION_PERIOD_REGEX = "\\d{1,3}(\\.\\d+)?";
 
     public void checkEdition(Edition edition) throws EditionValidatorException {
         if (edition == null) {
@@ -22,49 +18,48 @@ public class EditionValidator {
         }
 
         checkName(edition.getName());
-        //checkType(edition.getType());
-        //checkThemeId
-        //todo validator
+        checkTypeId(edition.getTypeId());
+        checkThemeId(edition.getThemeId());
+        checkPeriodicityPerYear(edition.getPeriodicityPerYear());
+        checkMinimumSubscriptionPeriodInMonths(edition.getMinimumSubscriptionPeriodInMonths());
+        checkPriceForMinimumSubscriptionPeriod(edition.getPriceForMinimumSubscriptionPeriod());
+
     }
 
     private void checkName(String name) throws EditionValidatorException {
-        if (name == null || !isStringMatchesRegex(name, NAME_REGEX)) {
+        if (name == null || !isStringMatchesRegex(name)) {
             throw new EditionValidatorException(ResourceBundleMessages.INVALID_NAME.getKey());
         }
     }
 
-    private void checkTypeId(String typeId) throws EditionValidatorException {
-        if (typeId == null || !isStringMatchesRegex(typeId, TYPE_REGEX)) {
-            throw new EditionValidatorException(ResourceBundleMessages.INVALID_TYPE_ID.getKey());
-        }
+    private void checkTypeId(int typeId) throws EditionValidatorException {
+        //todo get from type service list of type ids and check if contains
     }
 
-    private void checkThemeId(String themeId) throws EditionValidatorException {
-        if (themeId == null || !isStringMatchesRegex(themeId, THEME_ID_REGEX)) {
-            throw new EditionValidatorException(ResourceBundleMessages.INVALID_THEME_ID.getKey());
-        }
+    private void checkThemeId(int themeId) throws EditionValidatorException {
+        //todo get from theme service list of theme ids and check if contains
     }
 
-    private void checkPeriodicityPerYear(String periodicityPerYear) throws EditionValidatorException {
-        if (periodicityPerYear == null || !isStringMatchesRegex(periodicityPerYear, PERIODICITY_PER_YEAR_REGEX)) {
+    private void checkPeriodicityPerYear(int periodicityPerYear) throws EditionValidatorException {
+        if (periodicityPerYear < 1) {
             throw new EditionValidatorException(ResourceBundleMessages.INVALID_PERIODICITY_PER_YEAR.getKey());
         }
     }
 
-    private void checkMinimumSubscriptionPeriodInMonths(String minimumSubscriptionPeriodInMonths) throws EditionValidatorException {
-        if (minimumSubscriptionPeriodInMonths == null || !isStringMatchesRegex(minimumSubscriptionPeriodInMonths, MINIMUM_SUBSCRIPTION_PERIOD_IN_MONTHS_REGEX)) {
+    private void checkMinimumSubscriptionPeriodInMonths(int minimumSubscriptionPeriodInMonths) throws EditionValidatorException {
+        if (minimumSubscriptionPeriodInMonths < 1) {
             throw new EditionValidatorException(ResourceBundleMessages.INVALID_MINIMUM_SUBSCRIPTION_PERIOD.getKey());
         }
     }
 
-    private void checkPriceForMinimumSubscriptionPeriod(String priceForMinimumSubscriptionPeriod) throws EditionValidatorException {
-        if (priceForMinimumSubscriptionPeriod == null || !isStringMatchesRegex(priceForMinimumSubscriptionPeriod, PRICE_FOR_MINIMUM_SUBSCRIPTION_PERIOD_REGEX)) {
+    private void checkPriceForMinimumSubscriptionPeriod(BigDecimal priceForMinimumSubscriptionPeriod) throws EditionValidatorException {
+        if (priceForMinimumSubscriptionPeriod == null || priceForMinimumSubscriptionPeriod.compareTo(BigDecimal.valueOf(0)) < 1 ) {
             throw new EditionValidatorException(ResourceBundleMessages.INVALID_PRICE_FOR_MINIMUM_SUBSCRIPTION_PERIOD.getKey());
         }
     }
 
-    private boolean isStringMatchesRegex(String string, String regex) {
-        Pattern pattern = Pattern.compile(regex);
+    private boolean isStringMatchesRegex(String string) {
+        Pattern pattern = Pattern.compile(EditionValidator.NAME_REGEX);
         Matcher matcher = pattern.matcher(string);
         return matcher.matches();
     }
