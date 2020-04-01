d3d9f0589ffeafd9ac81d27f141f63979206cbe5
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-01 11:33:55 +0300

    Add theme service interface and implementation

---
+++ src/main/java/by/kiselevich/periodicals/service/theme/ThemeService.java
@@ -0,0 +1,11 @@
+package by.kiselevich.periodicals.service.theme;
+
+import by.kiselevich.periodicals.entity.Theme;
+import by.kiselevich.periodicals.exception.ServiceException;
+
+import java.util.List;
+
+public interface ThemeService {
+
+    List<Theme> getAllThemes() throws ServiceException;
+}
---
+++ src/main/java/by/kiselevich/periodicals/service/theme/ThemeServiceImpl.java
@@ -0,0 +1,34 @@
+package by.kiselevich.periodicals.service.theme;
+
+import by.kiselevich.periodicals.command.ResourceBundleMessages;
+import by.kiselevich.periodicals.entity.Theme;
+import by.kiselevich.periodicals.exception.RepositoryException;
+import by.kiselevich.periodicals.exception.ServiceException;
+import by.kiselevich.periodicals.factory.RepositoryFactory;
+import by.kiselevich.periodicals.repository.theme.ThemeRepository;
+import by.kiselevich.periodicals.specification.theme.FindAllThemes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public class ThemeServiceImpl implements ThemeService {
+
+    private static final Logger LOG = LogManager.getLogger(ThemeServiceImpl.class);
+
+    private ThemeRepository themeRepository;
+
+    public ThemeServiceImpl() {
+        themeRepository = RepositoryFactory.getInstance().getThemeRepository();
+    }
+
+    @Override
+    public List<Theme> getAllThemes() throws ServiceException {
+        try {
+            return themeRepository.query(new FindAllThemes());
+        } catch (RepositoryException e) {
+            LOG.warn(e);
+            throw new ServiceException(ResourceBundleMessages.INTERNAL_ERROR.getKey());
+        }
+    }
+}
