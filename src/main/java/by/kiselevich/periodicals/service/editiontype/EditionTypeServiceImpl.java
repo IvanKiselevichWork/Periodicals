ced1b1f91bc332e051dff6998e9135640b22875f
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-01 15:10:56 +0300

    Add edition type service interface and implementation

---
+++ src/main/java/by/kiselevich/periodicals/service/editiontype/EditionTypeServiceImpl.java
@@ -0,0 +1,34 @@
+package by.kiselevich.periodicals.service.editiontype;
+
+import by.kiselevich.periodicals.command.ResourceBundleMessages;
+import by.kiselevich.periodicals.entity.EditionType;
+import by.kiselevich.periodicals.exception.RepositoryException;
+import by.kiselevich.periodicals.exception.ServiceException;
+import by.kiselevich.periodicals.factory.RepositoryFactory;
+import by.kiselevich.periodicals.repository.editiontype.EditionTypeRepository;
+import by.kiselevich.periodicals.specification.editiontype.FindAllEditionsTypes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public class EditionTypeServiceImpl implements EditionTypeService {
+
+    private static final Logger LOG = LogManager.getLogger(EditionTypeServiceImpl.class);
+
+    private EditionTypeRepository editionTypeRepository;
+
+    public EditionTypeServiceImpl() {
+        editionTypeRepository = RepositoryFactory.getInstance().getEditionTypeRepository();
+    }
+
+    @Override
+    public List<EditionType> getAllEditionsTypes() throws ServiceException {
+        try {
+            return editionTypeRepository.query(new FindAllEditionsTypes());
+        } catch (RepositoryException e) {
+            LOG.warn(e);
+            throw new ServiceException(ResourceBundleMessages.INTERNAL_ERROR.getKey());
+        }
+    }
+}
