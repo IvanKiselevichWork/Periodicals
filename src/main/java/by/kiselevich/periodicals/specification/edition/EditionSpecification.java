f585c78a9bf9e0a9dc74c9b2222776dfb4287496
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-04 16:03:32 +0300

    Move methods for getting entities from result set to one class from all specification interfaces

--- src/main/java/by/kiselevich/periodicals/specification/edition/EditionSpecification.java
+++ src/main/java/by/kiselevich/periodicals/specification/edition/EditionSpecification.java
@@ -3,36 +3,5 @@ package by.kiselevich.periodicals.specification.edition;
 import by.kiselevich.periodicals.entity.Edition;
 import by.kiselevich.periodicals.specification.Specification;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 public interface EditionSpecification extends Specification<Edition> {
-
-    String ID = "id";
-    String NAME = "name";
-    String TYPE_ID = "type_id";
-    String THEME_ID = "theme_id";
-    String PERIODICITY_PER_YEAR = "periodicity_per_year";
-    String MINIMUM_SUBSCRIPTION_PERIOD_IN_MONTHS = "minimum_subscription_period_in_months";
-    String PRICE_FOR_MINIMUM_SUBSCRIPTION_PERIOD = "price_for_minimum_subscription_period";
-
-
-    default List<Edition> getEditionFromResultSet(ResultSet resultSet) throws SQLException {
-        List<Edition> editions = new ArrayList<>();
-        while (resultSet.next()) {
-            editions.add(new Edition.EditionBuilder()
-                    .id(resultSet.getInt(ID))
-                    .name(resultSet.getString(NAME))
-                    .typeId(resultSet.getInt(TYPE_ID))
-                    .themeId(resultSet.getInt(THEME_ID))
-                    .periodicityPerYear(resultSet.getInt(PERIODICITY_PER_YEAR))
-                    .minimumSubscriptionPeriodInMonths(resultSet.getInt(MINIMUM_SUBSCRIPTION_PERIOD_IN_MONTHS))
-                    .priceForMinimumSubscriptionPeriod(resultSet.getBigDecimal(PRICE_FOR_MINIMUM_SUBSCRIPTION_PERIOD))
-                    .build()
-            );
-        }
-        return editions;
-    }
 }
--- src/main/java/by/kiselevich/periodicals/specification/theme/ThemeSpecification.java
+++ src/main/java/by/kiselevich/periodicals/specification/theme/ThemeSpecification.java
@@ -3,25 +3,5 @@ package by.kiselevich.periodicals.specification.theme;
 import by.kiselevich.periodicals.entity.EditionTheme;
 import by.kiselevich.periodicals.specification.Specification;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 public interface ThemeSpecification extends Specification<EditionTheme> {
-
-    String ID = "id";
-    String TITLE = "title";
-
-    default List<EditionTheme> getThemeFromResultSet(ResultSet resultSet) throws SQLException {
-        List<EditionTheme> editionThemes = new ArrayList<>();
-        while (resultSet.next()) {
-            editionThemes.add(new EditionTheme.EditionThemeBuilder()
-                    .id(resultSet.getInt(ID))
-                    .title(resultSet.getString(TITLE))
-                    .build()
-            );
-        }
-        return editionThemes;
-    }
 }
