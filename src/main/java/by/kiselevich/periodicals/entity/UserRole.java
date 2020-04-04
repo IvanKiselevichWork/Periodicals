489afa3a9e054db756ae8b1f584dd23c3aeb9aaf
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-04 11:36:09 +0300

    Add UserRole entity

---
+++ src/main/java/by/kiselevich/periodicals/entity/UserRole.java
@@ -0,0 +1,83 @@
+package by.kiselevich.periodicals.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UserRole implements Serializable {
+
+    private static final long serialVersionUID = -8285019634206760939L;
+
+    private int id;
+    private String role;
+
+    public UserRole() {
+
+    }
+
+    public UserRole(int id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public static class UserRoleBuilder {
+
+        private int id;
+        private String role;
+
+        public UserRoleBuilder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserRoleBuilder id(String role) {
+            this.role = role;
+            return this;
+        }
+
+        public UserRole build() {
+            return new UserRole(id, role);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserRole that = (UserRole) o;
+
+        if (id != that.id) return false;
+        return Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
