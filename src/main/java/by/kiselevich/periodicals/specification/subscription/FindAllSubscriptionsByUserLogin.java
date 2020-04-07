0847c515c730494037c4a8237cbfc8d3dd9f20b3
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-07 11:24:52 +0300

    Add find all subscriptions by user login repository specification

---
+++ src/main/java/by/kiselevich/periodicals/specification/subscription/FindAllSubscriptionsByUserLogin.java
@@ -0,0 +1,43 @@
+package by.kiselevich.periodicals.specification.subscription;
+
+import by.kiselevich.periodicals.entity.Subscription;
+import by.kiselevich.periodicals.exception.RepositoryException;
+import by.kiselevich.periodicals.pool.ConnectionPool;
+import by.kiselevich.periodicals.pool.ConnectionProxy;
+import by.kiselevich.periodicals.specification.SpecificationUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllSubscriptionsByUserLogin extends SpecificationUtil implements SubscriptionSpecification {
+
+    private static final String FIND_ALL_SUBSCRIPTION_BY_USER_LOGIN = "select * from subscription inner join edition on subscription.edition_id = edition.id inner  join edition_type on edition.type_id = edition_type.id inner join edition_theme on edition.theme_id = edition_theme.id inner join user on subscription.user_id = user.id inner join user_role on user.role_id = user_role.id where user.login = ?";
+
+    private String userLogin;
+
+    public FindAllSubscriptionsByUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    @Override
+    public List<Subscription> query() throws RepositoryException {
+        ResultSet resultSet = null;
+        List<Subscription> subscriptions = new ArrayList<>();
+        try (ConnectionProxy connection = ConnectionPool.INSTANCE.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(FIND_ALL_SUBSCRIPTION_BY_USER_LOGIN);
+            statement.setString(1, userLogin);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                subscriptions.add(getSubscriptionFromResultSet(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new RepositoryException(e);
+        } finally {
+            closeResultSet(resultSet);
+        }
+        return subscriptions;
+    }
+}
