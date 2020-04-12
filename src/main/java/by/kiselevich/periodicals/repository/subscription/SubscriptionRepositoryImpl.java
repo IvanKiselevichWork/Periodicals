86ce9169c857b1eae0382f70d98eb4797cad26c2
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-12 12:36:24 +0300

    Add implementation of add method to subscription repository

--- src/main/java/by/kiselevich/periodicals/repository/subscription/SubscriptionRepositoryImpl.java
+++ src/main/java/by/kiselevich/periodicals/repository/subscription/SubscriptionRepositoryImpl.java
@@ -2,19 +2,49 @@ package by.kiselevich.periodicals.repository.subscription;
 
 import by.kiselevich.periodicals.entity.Subscription;
 import by.kiselevich.periodicals.exception.RepositoryException;
+import by.kiselevich.periodicals.pool.ConnectionPool;
+import by.kiselevich.periodicals.pool.ConnectionProxy;
 import by.kiselevich.periodicals.specification.Specification;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class SubscriptionRepositoryImpl implements SubscriptionRepository {
 
-    private static final Logger LOG = LogManager.getLogger(SubscriptionRepositoryImpl.class);
+    private static final String ADD_SUBSCRIPTION = "insert into subscription(edition_id, subscription_start_date, subscription_end_date, user_id, is_paid) values (?, ?, ?, ?, ?)";
+
+    private static final String SUBSCRIPTION_NOT_ADDED_MESSAGE = "Subscription has not been added";
 
     @Override
     public void add(Subscription subscription) throws RepositoryException {
-        //todo
+        ResultSet generatedId = null;
+        try (ConnectionProxy connection = ConnectionPool.INSTANCE.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(ADD_SUBSCRIPTION, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, subscription.getEdition().getId());
+            statement.setTimestamp(2, subscription.getSubscriptionStartDate());
+            statement.setTimestamp(3, subscription.getSubscriptionEndDate());
+            statement.setInt(4, subscription.getUser().getId());
+            statement.setBoolean(5, subscription.isPaid());
+            int updatedRowCount = statement.executeUpdate();
+            boolean isSubscriptionAdded = false;
+            if (updatedRowCount == 1) {
+                generatedId = statement.getGeneratedKeys();
+                if (generatedId.next()) {
+                    subscription.setId(generatedId.getInt(1));
+                    isSubscriptionAdded = true;
+                }
+            }
+            if (!isSubscriptionAdded) {
+                throw new RepositoryException(SUBSCRIPTION_NOT_ADDED_MESSAGE);
+            }
+        } catch (SQLException e) {
+            throw new RepositoryException(e);
+        } finally {
+            closeResultSet(generatedId);
+        }
     }
 
     @Override
