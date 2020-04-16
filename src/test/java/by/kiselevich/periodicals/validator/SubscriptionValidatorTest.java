c8e788b484122f5320461e25286ce58136d08569
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-16 16:11:01 +0300

    Add SubscriptionValidator tests

---
+++ src/test/java/by/kiselevich/periodicals/validator/SubscriptionValidatorTest.java
@@ -0,0 +1,82 @@
+package by.kiselevich.periodicals.validator;
+
+import by.kiselevich.periodicals.entity.*;
+import by.kiselevich.periodicals.exception.NoJDBCDriverException;
+import by.kiselevich.periodicals.exception.NoJDBCPropertiesException;
+import by.kiselevich.periodicals.exception.ServiceException;
+import by.kiselevich.periodicals.exception.ValidatorException;
+import by.kiselevich.periodicals.factory.ServiceFactory;
+import by.kiselevich.periodicals.pool.ConnectionPoolImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class SubscriptionValidatorTest extends Assert {
+
+    private static final SubscriptionValidator subscriptionValidator = new SubscriptionValidator();
+
+    @Before
+    public void init() throws NoJDBCDriverException, NoJDBCPropertiesException {
+        ConnectionPoolImpl.INSTANCE.initPool();
+    }
+
+    @After
+    public void deInit() {
+        ConnectionPoolImpl.INSTANCE.deInitPool();
+    }
+
+    @Test
+    public void testCheckSubscriptionPositive() throws ServiceException, ValidatorException {
+        Subscription subscription = getSubscription();
+        subscriptionValidator.checkSubscription(subscription);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void testCheckSubscriptionNullSubscription() throws ValidatorException {
+        subscriptionValidator.checkSubscription(null);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void testCheckSubscriptionInvalidEdition() throws ServiceException, ValidatorException {
+        Subscription subscription = getSubscription();
+        subscription.getEdition().setName("Invalid edition");
+        subscriptionValidator.checkSubscription(subscription);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void testCheckSubscriptionInvalidPeriod1() throws ServiceException, ValidatorException {
+        Subscription subscription = getSubscription();
+        subscription.setSubscriptionEndDate(Timestamp.valueOf(LocalDateTime.parse("2009-12-25T10:15:30")));
+        subscriptionValidator.checkSubscription(subscription);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void testCheckSubscriptionInvalidPeriod2() throws ServiceException, ValidatorException {
+        Subscription subscription = getSubscription();
+        subscription.setSubscriptionEndDate(Timestamp.valueOf(LocalDateTime.parse("2007-11-25T10:15:30")));
+        subscriptionValidator.checkSubscription(subscription);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void testCheckSubscriptionInvalidUser() throws ServiceException, ValidatorException {
+        Subscription subscription = getSubscription();
+        subscription.getUser().setLogin("invalid user login");
+        subscriptionValidator.checkSubscription(subscription);
+    }
+
+    private Subscription getSubscription() throws ServiceException {
+        Edition edition = ServiceFactory.getInstance().getEditionService().getAllEditions().get(0);
+        User user = ServiceFactory.getInstance().getUserService().getAllUsers().get(0);
+        return new Subscription.SubscriptionBuilder()
+                .id(0)
+                .edition(edition)
+                .subscriptionStartDate(Timestamp.valueOf(LocalDateTime.parse("2007-12-25T10:15:30")))
+                .subscriptionEndDate(Timestamp.valueOf(LocalDateTime.parse("2008-12-25T10:15:30")))
+                .user(user)
+                .build();
+    }
+}
