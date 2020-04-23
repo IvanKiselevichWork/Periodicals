f9f4c14098a3f558dfec83918f95e076ea515afb
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-23 14:20:51 +0300

    Replace code duplacation in EditionRepositoryImpl with private method

--- src/main/java/by/kiselevich/periodicals/repository/edition/EditionRepositoryImpl.java
+++ src/main/java/by/kiselevich/periodicals/repository/edition/EditionRepositoryImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class EditionRepositoryImpl extends RepositoryUtil implements EditionRepository {
 
     private static final String ADD_EDITION = "insert into edition (name, type_id, theme_id, periodicity_per_year, minimum_subscription_period_in_months, price_for_minimum_subscription_period, is_blocked) values (?, ?, ?, ?, ?, ?, ?)";
-    private static final String UPDATE_EDITION = "update edition set name = ?, type_id = ?, theme_id = ?, periodicity_per_year = ?, minimum_subscription_period_in_months = ?, price_for_minimum_subscription_period = ? where id = ?";
+    private static final String UPDATE_EDITION = "update edition set name = ?, type_id = ?, theme_id = ?, periodicity_per_year = ?, minimum_subscription_period_in_months = ?, price_for_minimum_subscription_period = ?, is_blocked = ? where id = ?";
 
     private static final String BLOCK_EDITION = "update edition set is_blocked = 1 where id = ?";
     private static final String UNBLOCK_EDITION = "update edition set is_blocked = 0 where id = ?";
@@ -32,13 +32,7 @@ public class EditionRepositoryImpl extends RepositoryUtil implements EditionRepo
     public void add(Edition edition) throws RepositoryException {
         try (Connection connection = connectionPool.getConnection();
              PreparedStatement statement = connection.prepareStatement(ADD_EDITION, Statement.RETURN_GENERATED_KEYS)) {
-            statement.setString(1, edition.getName());
-            statement.setInt(2, edition.getEditionType().getId());
-            statement.setInt(3, edition.getEditionTheme().getId());
-            statement.setInt(4, edition.getPeriodicityPerYear());
-            statement.setInt(5, edition.getMinimumSubscriptionPeriodInMonths());
-            statement.setBigDecimal(6, edition.getPriceForMinimumSubscriptionPeriod());
-            statement.setBoolean(7, edition.isBlocked());
+            setEditionInPreparedStatement(edition, statement);
             int generatedId = executeUpdateOneRowAndGetGeneratedId(statement, EDITION_NOT_ADDED_MESSAGE);
             edition.setId(generatedId);
         } catch (SQLException e) {
@@ -60,13 +54,8 @@ public class EditionRepositoryImpl extends RepositoryUtil implements EditionRepo
     public void update(Edition edition) throws RepositoryException {
         try (Connection connection = connectionPool.getConnection();
              PreparedStatement statement = connection.prepareStatement(UPDATE_EDITION)) {
-            statement.setString(1, edition.getName());
-            statement.setInt(2, edition.getEditionType().getId());
-            statement.setInt(3, edition.getEditionTheme().getId());
-            statement.setInt(4, edition.getPeriodicityPerYear());
-            statement.setInt(5, edition.getMinimumSubscriptionPeriodInMonths());
-            statement.setBigDecimal(6, edition.getPriceForMinimumSubscriptionPeriod());
-            statement.setInt(7, edition.getId());
+            setEditionInPreparedStatement(edition, statement);
+            statement.setInt(8, edition.getId());
             int updatedRowCount = statement.executeUpdate();
             if (updatedRowCount != 1) {
                 throw new RepositoryException(EDITION_NOT_UPDATED_MESSAGE);
@@ -79,5 +68,15 @@ public class EditionRepositoryImpl extends RepositoryUtil implements EditionRepo
     @Override
     public List<Edition> query(Specification<Edition> specification) throws RepositoryException {
         return specification.query();
+    }
+
+    private void setEditionInPreparedStatement(Edition edition, PreparedStatement statement) throws SQLException {
+        statement.setString(1, edition.getName());
+        statement.setInt(2, edition.getEditionType().getId());
+        statement.setInt(3, edition.getEditionTheme().getId());
+        statement.setInt(4, edition.getPeriodicityPerYear());
+        statement.setInt(5, edition.getMinimumSubscriptionPeriodInMonths());
+        statement.setBigDecimal(6, edition.getPriceForMinimumSubscriptionPeriod());
+        statement.setBoolean(7, edition.isBlocked());
     }
 }
