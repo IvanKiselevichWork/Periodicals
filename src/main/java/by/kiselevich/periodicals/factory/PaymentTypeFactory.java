d622367199d969e03dd6db0aa809d1cf56a475cc
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-23 10:31:27 +0300

    Add PaymentType factory

---
+++ src/main/java/by/kiselevich/periodicals/factory/PaymentTypeFactory.java
@@ -0,0 +1,30 @@
+package by.kiselevich.periodicals.factory;
+
+import by.kiselevich.periodicals.entity.PaymentType;
+
+public class PaymentTypeFactory {
+    private static final int REFILL_ID = 1;
+    private static final int PAYMENT_ID = 2;
+    private static final String REFILL_TYPE = "refill";
+    private static final String PAYMENT_TYPE = "payment";
+
+    private PaymentTypeFactory() {}
+
+    private static final PaymentType REFILL = new PaymentType.PaymentTypeBuilder()
+            .id(REFILL_ID)
+            .type(REFILL_TYPE)
+            .build();
+
+    private static final PaymentType PAYMENT = new PaymentType.PaymentTypeBuilder()
+            .id(PAYMENT_ID)
+            .type(PAYMENT_TYPE)
+            .build();
+
+    public static PaymentType getRefill() {
+        return REFILL;
+    }
+
+    public static PaymentType getPayment() {
+        return PAYMENT;
+    }
+}
