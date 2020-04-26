81d6f2bb2869b7a293d8e1ffe9206a899828ad56
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-26 20:49:04 +0300

    Add javadoc for MailService

--- src/main/java/by/kiselevich/periodicals/service/mail/MailService.java
+++ src/main/java/by/kiselevich/periodicals/service/mail/MailService.java
@@ -7,5 +7,10 @@ import javax.servlet.http.HttpSession;
 
 public interface MailService {
 
+    /**
+     * Sends registration letter to {@link User} with locate from {@link HttpSession}
+     * @param user {@link User} to which letter send
+     * @param session {@link HttpSession} of {@link User}
+     */
     void sendRegistrationLetter(User user, HttpSession session);
 }
