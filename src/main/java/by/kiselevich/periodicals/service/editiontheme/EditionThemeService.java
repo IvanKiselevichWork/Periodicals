f5742e9bc30f675863dc91d4e6d7f8f2a3f8d71c
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-26 20:59:47 +0300

    Add javadoc for EditionThemeService

--- src/main/java/by/kiselevich/periodicals/service/editiontheme/EditionThemeService.java
+++ src/main/java/by/kiselevich/periodicals/service/editiontheme/EditionThemeService.java
@@ -1,5 +1,6 @@
 package by.kiselevich.periodicals.service.editiontheme;
 
+import by.kiselevich.periodicals.command.ResourceBundleMessages;
 import by.kiselevich.periodicals.entity.EditionTheme;
 import by.kiselevich.periodicals.exception.ServiceException;
 
@@ -7,7 +8,18 @@ import java.util.List;
 
 public interface EditionThemeService {
 
+    /**
+     * Returns all {@link EditionTheme} from data source
+     * @return {@link List} with all {@link EditionTheme}
+     * @throws ServiceException with {@link ResourceBundleMessages} key as message to view error message to user if error occurs
+     */
     List<EditionTheme> getAllThemes() throws ServiceException;
 
+    /**
+     * Returns {@link EditionTheme} from data source  by its {@code id}
+     * @param id {@link EditionTheme} {@code id}
+     * @return {@link List} with {@link EditionTheme}
+     * @throws ServiceException with {@link ResourceBundleMessages} key as message to view error message to user if error occurs
+     */
     List<EditionTheme> getThemeById(int id) throws ServiceException;
 }
