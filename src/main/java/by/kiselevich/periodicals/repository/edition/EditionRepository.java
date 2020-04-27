971a6726e0258c4d3477c74c888dc6c43d473c42
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-27 11:51:16 +0300

    Add javadoc for EditionRepository

--- src/main/java/by/kiselevich/periodicals/repository/edition/EditionRepository.java
+++ src/main/java/by/kiselevich/periodicals/repository/edition/EditionRepository.java
@@ -6,11 +6,31 @@ import by.kiselevich.periodicals.repository.Repository;
 
 public interface EditionRepository extends Repository<Edition> {
 
+    /**
+     * Adds {@code Edition} to data source
+     * @param edition {@link Edition} to add
+     * @throws RepositoryException if error occurs
+     */
     void add(Edition edition) throws RepositoryException;
 
+    /**
+     * Updates {@code Edition} in data source
+     * @param edition {@link Edition} to update
+     * @throws RepositoryException if error occurs
+     */
     void update(Edition edition) throws RepositoryException;
 
+    /**
+     * Blocks {@code Edition}
+     * @param id {@link Edition} {@code id}
+     * @throws RepositoryException if error occurs
+     */
     void block(int id) throws RepositoryException;
 
+    /**
+     * Unblocks {@code Edition}
+     * @param id {@link Edition} {@code id}
+     * @throws RepositoryException if error occurs
+     */
     void unblock(int id) throws RepositoryException;
 }
