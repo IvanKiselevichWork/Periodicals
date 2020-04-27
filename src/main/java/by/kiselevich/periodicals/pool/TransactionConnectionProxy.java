6648576760258ef35c5f5586dd27804e0819fbeb
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-27 11:55:26 +0300

    Add javadoc for TransactionConnectionProxy

--- src/main/java/by/kiselevich/periodicals/pool/TransactionConnectionProxy.java
+++ src/main/java/by/kiselevich/periodicals/pool/TransactionConnectionProxy.java
@@ -2,6 +2,10 @@ package by.kiselevich.periodicals.pool;
 
 import java.sql.Connection;
 
+/**
+ * Class for working with {@link Connection} in transactions.
+ * Method {@code close()} do nothing, so it can be used in {@code try-with-resources} statements
+ */
 public class TransactionConnectionProxy extends ConnectionProxy {
 
     public TransactionConnectionProxy(Connection connection) {
