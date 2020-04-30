8a1c21ece0576b58e40332b156d3e52bc33bc6f7
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-04-30 21:59:06 +0300

    Make getEditionFromRequest method build full Edition exclude id

--- src/main/java/by/kiselevich/periodicals/command/admin/AbstractEditionCommand.java
+++ src/main/java/by/kiselevich/periodicals/command/admin/AbstractEditionCommand.java
@@ -1,31 +1,49 @@
 package by.kiselevich.periodicals.command.admin;
 
 import by.kiselevich.periodicals.command.JspParameter;
+import by.kiselevich.periodicals.command.ResourceBundleMessages;
 import by.kiselevich.periodicals.entity.Edition;
 import by.kiselevich.periodicals.entity.EditionTheme;
 import by.kiselevich.periodicals.entity.EditionType;
+import by.kiselevich.periodicals.exception.ServiceException;
+import by.kiselevich.periodicals.factory.ServiceFactory;
+import by.kiselevich.periodicals.service.editiontheme.EditionThemeService;
+import by.kiselevich.periodicals.service.editiontype.EditionTypeService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class AbstractEditionCommand {
 
-    protected Edition getEditionFromRequest(HttpServletRequest req) {
+    protected final EditionTypeService editionTypeService;
+    protected final EditionThemeService editionThemeService;
+
+    public AbstractEditionCommand() {
+        editionTypeService = ServiceFactory.getInstance().getEditionTypeService();
+        editionThemeService = ServiceFactory.getInstance().getEditionThemeService();
+    }
+
+    protected Edition getEditionFromRequest(HttpServletRequest req) throws ServiceException {
         String name = req.getParameter(JspParameter.NAME.getValue());
         int typeId = Integer.parseInt(req.getParameter(JspParameter.TYPE_ID.getValue()));
+        List<EditionType> editionTypeList = editionTypeService.getEditionTypeById(typeId);
+        if (editionTypeList.isEmpty()) {
+            throw new ServiceException(ResourceBundleMessages.INVALID_TYPE.getKey());
+        }
         int themeId = Integer.parseInt(req.getParameter(JspParameter.THEME_ID.getValue()));
+        List<EditionTheme> editionThemeList = editionThemeService.getThemeById(themeId);
+        if (editionThemeList.isEmpty()) {
+            throw new ServiceException(ResourceBundleMessages.INVALID_THEME.getKey());
+        }
         int periodicityPerYear = Integer.parseInt(req.getParameter(JspParameter.PERIODICITY_PER_YEAR.getValue()));
         int minimumSubscriptionPeriodInMonths = Integer.parseInt(req.getParameter(JspParameter.MINIMUM_SUBSCRIPTION_PERIOD.getValue()));
         BigDecimal priceForMinimumSubscriptionPeriod = BigDecimal.valueOf(Double.parseDouble(req.getParameter(JspParameter.PRICE_FOR_MINIMUM_SUBSCRIPTION_PERIOD.getValue())));
 
         return new Edition.EditionBuilder()
                 .name(name)
-                .editionType(new EditionType.EditionTypeBuilder()
-                        .id(typeId)
-                        .build())
-                .editionTheme(new EditionTheme.EditionThemeBuilder()
-                        .id(themeId)
-                        .build())
+                .editionType(editionTypeList.get(0))
+                .editionTheme(editionThemeList.get(0))
                 .periodicityPerYear(periodicityPerYear)
                 .minimumSubscriptionPeriodInMonths(minimumSubscriptionPeriodInMonths)
                 .priceForMinimumSubscriptionPeriod(priceForMinimumSubscriptionPeriod)
