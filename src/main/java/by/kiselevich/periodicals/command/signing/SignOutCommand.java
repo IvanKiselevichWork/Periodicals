372778f191233d552f0ae703d8efa1113965e210
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-05-01 13:55:19 +0300

    Add session invalidation and redirection to home page on sign out

--- src/main/java/by/kiselevich/periodicals/command/signing/SignOutCommand.java
+++ src/main/java/by/kiselevich/periodicals/command/signing/SignOutCommand.java
@@ -1,19 +1,26 @@
 package by.kiselevich.periodicals.command.signing;
 
-import by.kiselevich.periodicals.command.Attribute;
 import by.kiselevich.periodicals.command.Command;
 import by.kiselevich.periodicals.command.Page;
-import by.kiselevich.periodicals.command.UserType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SignOutCommand implements Command {
 
+    private static final Logger LOG = LogManager.getLogger(SignOutCommand.class);
+
     @Override
     public Page execute(HttpServletRequest req, HttpServletResponse resp) {
-        req.getSession().setAttribute(Attribute.USER_TYPE.getValue(), UserType.GUEST);
-        req.getSession().setAttribute(Attribute.LOGIN.getValue(), null);
-        return Page.HOME_PAGE;
+        req.getSession().invalidate();
+        try {
+            resp.sendRedirect(req.getContextPath());
+        } catch (IOException e) {
+            LOG.error(e);
+        }
+        return Page.EMPTY_PAGE;
     }
 }
