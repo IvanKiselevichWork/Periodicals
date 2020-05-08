ad527934d67655299c1d013c4b4646c7737452cc
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-05-09 00:42:23 +0300

    Add refund payment type to pre-build payment types

--- src/main/java/by/kiselevich/periodicals/factory/PaymentTypeFactory.java
+++ src/main/java/by/kiselevich/periodicals/factory/PaymentTypeFactory.java
@@ -3,13 +3,15 @@ package by.kiselevich.periodicals.factory;
 import by.kiselevich.periodicals.entity.PaymentType;
 
 /**
- * Class with pre-build {@link PaymentType} objects: refill and payment
+ * Class with pre-build {@link PaymentType} objects: refill, payment and refund
  */
 public class PaymentTypeFactory {
     private static final int REFILL_ID = 1;
     private static final int PAYMENT_ID = 2;
+    private static final int REFUND_ID = 3;
     private static final String REFILL_TYPE = "refill";
     private static final String PAYMENT_TYPE = "payment";
+    private static final String REFUND_TYPE = "refund";
 
     private PaymentTypeFactory() {}
 
@@ -23,11 +25,20 @@ public class PaymentTypeFactory {
             .type(PAYMENT_TYPE)
             .build();
 
+    private static final PaymentType REFUND = new PaymentType.PaymentTypeBuilder()
+            .id(REFUND_ID)
+            .type(REFUND_TYPE)
+            .build();
+
     public static PaymentType getRefill() {
         return REFILL;
     }
 
     public static PaymentType getPayment() {
         return PAYMENT;
+    }
+
+    public static PaymentType getRefund() {
+        return REFUND;
     }
 }
