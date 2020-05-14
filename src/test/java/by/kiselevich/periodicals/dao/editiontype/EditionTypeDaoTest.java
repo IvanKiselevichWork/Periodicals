054b406d786a150b0b386e95f9b0bdcb07fb6163
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-05-14 23:05:40 +0300

    Add tests for EditionTypeDao

---
+++ src/test/java/by/kiselevich/periodicals/dao/editiontype/EditionTypeDaoTest.java
@@ -0,0 +1,36 @@
+package by.kiselevich.periodicals.dao.editiontype;
+
+import by.kiselevich.periodicals.entity.EditionType;
+import by.kiselevich.periodicals.exception.DaoException;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+public class EditionTypeDaoTest {
+
+    private static Session session;
+
+    @BeforeClass
+    public static void init() {
+        session = new Configuration().configure().buildSessionFactory().openSession();
+    }
+
+    @AfterClass
+    public static void deInit() {
+        if (session != null && session.isOpen()) {
+            session.close();
+        }
+    }
+
+    @Test
+    public void EditionTypeDaoTest1() throws DaoException {
+        EditionTypeDao editionTypeDao = new EditionTypeDaoImpl(session);
+        List<EditionType> editionTypes = editionTypeDao.getAllEditionTypes();
+        Assert.assertEquals(2, editionTypes.size());
+    }
+}
