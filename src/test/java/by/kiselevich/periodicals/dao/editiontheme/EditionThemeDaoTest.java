a37c8403e539282157f451db4e41efda60c2fed2
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-05-15 20:07:24 +0300

    Add EditionThemeDao tests

---
+++ src/test/java/by/kiselevich/periodicals/dao/editiontheme/EditionThemeDaoTest.java
@@ -0,0 +1,36 @@
+package by.kiselevich.periodicals.dao.editiontheme;
+
+import by.kiselevich.periodicals.entity.EditionTheme;
+import by.kiselevich.periodicals.exception.DaoException;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+public class EditionThemeDaoTest {
+
+    private static Session session;
+
+    @BeforeClass
+    public static void init() {
+        session = new Configuration().configure().buildSessionFactory().openSession();
+    }
+
+    @AfterClass
+    public static void deInit() {
+        if (session != null && session.isOpen()) {
+            session.close();
+        }
+    }
+
+    @Test
+    public void EditionThemeDaoTest1() throws DaoException {
+        EditionThemeDao editionThemeDao = new EditionThemeDaoImpl(session);
+        List<EditionTheme> editionThemes = editionThemeDao.getAllEditionThemes();
+        Assert.assertEquals(24, editionThemes.size());
+    }
+}
