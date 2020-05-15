fd2f3e3ca808344b3e7094847b85e7ceed143fb8
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-05-15 09:16:55 +0300

    Add tests for UserDao

---
+++ src/test/java/by/kiselevich/periodicals/dao/user/UserDaoTest.java
@@ -0,0 +1,57 @@
+package by.kiselevich.periodicals.dao.user;
+
+import by.kiselevich.periodicals.entity.User;
+import by.kiselevich.periodicals.exception.DaoException;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+
+    private static Session session;
+
+    @BeforeClass
+    public static void init() {
+        session = new Configuration().configure().buildSessionFactory().openSession();
+    }
+
+    @AfterClass
+    public static void deInit() {
+        if (session != null && session.isOpen()) {
+            session.close();
+        }
+    }
+
+    @Test
+    public void UserDaoTest1() throws DaoException {
+        UserDao userDao = new UserDaoImpl(session);
+        List<User> users = userDao.getAllUsers();
+        Assert.assertEquals(1000, users.size());
+    }
+
+    @Test
+    public void UserDaoTest2() throws DaoException {
+        UserDao userDao = new UserDaoImpl(session);
+        User user = userDao.getUserByLogin("user1");
+        Assert.assertEquals("user@periodicals.com", user.getEmail());
+    }
+
+    @Test
+    public void UserDaoTest3() throws DaoException {
+        UserDao userDao = new UserDaoImpl(session);
+        User user = userDao.getUserByLogin("user1");
+        Assert.assertEquals(5, user.getPayments().size());
+    }
+
+    @Test
+    public void UserDaoTest4() throws DaoException {
+        UserDao userDao = new UserDaoImpl(session);
+        User user = userDao.getUserByLogin("user1");
+        Assert.assertEquals(1004, user.getSubscriptions().size());
+    }
+}
