c0c5dfc6df30138c94fc81acf1d369c02fee8bdf
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-05-21 15:21:41 +0300

    Add Service annotation to PaymentServiceImpl and make all methods Transactional

--- src/main/java/by/kiselevich/periodicals/service/payment/PaymentServiceImpl.java
+++ src/main/java/by/kiselevich/periodicals/service/payment/PaymentServiceImpl.java
@@ -5,26 +5,29 @@ import by.kiselevich.periodicals.dao.payment.PaymentDao;
 import by.kiselevich.periodicals.entity.Payment;
 import by.kiselevich.periodicals.exception.DaoException;
 import by.kiselevich.periodicals.exception.ServiceException;
-import by.kiselevich.periodicals.factory.DaoFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Implementation of {@link PaymentService}
  */
+@Service
 public class PaymentServiceImpl implements PaymentService {
 
     private static final Logger LOG = LogManager.getLogger(PaymentServiceImpl.class);
 
-    private final PaymentDao paymentDao;
+    private PaymentDao paymentDao;
 
-    public PaymentServiceImpl() {
-        paymentDao = DaoFactory.getInstance().getPaymentDao();
+    public void setPaymentDao(PaymentDao paymentDao) {
+        this.paymentDao = paymentDao;
     }
 
     @Override
+    @Transactional
     public List<Payment> getAllPayments() throws ServiceException {
         try {
             return paymentDao.getAllPayments();
