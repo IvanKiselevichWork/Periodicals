31ec446cad5eac067677480928ed00a80af75deb
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-05-21 11:30:49 +0300

    Add Repository annotation to PaymentDaoImpl, replace Session with SessionFactory

--- src/main/java/by/kiselevich/periodicals/dao/payment/PaymentDaoImpl.java
+++ src/main/java/by/kiselevich/periodicals/dao/payment/PaymentDaoImpl.java
@@ -3,21 +3,25 @@ package by.kiselevich.periodicals.dao.payment;
 import by.kiselevich.periodicals.entity.Payment;
 import by.kiselevich.periodicals.exception.DaoException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class PaymentDaoImpl implements PaymentDao {
 
-    private Session session;
+    private SessionFactory sessionFactory;
 
-    public PaymentDaoImpl(Session session) {
-        this.session = session;
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
     public void add(Payment payment) throws DaoException {
         try {
-            session.save(payment);
+            Session session = sessionFactory.getCurrentSession();
+            session.persist(payment);
         } catch (Exception e) {
             throw new DaoException(e);
         }
@@ -26,7 +30,8 @@ public class PaymentDaoImpl implements PaymentDao {
     @Override
     public List<Payment> getAllPayments() throws DaoException {
         try {
-            return session.createQuery("select p from Payment p", Payment.class).list();
+            Session session = sessionFactory.getCurrentSession();
+            return session.createQuery("from Payment", Payment.class).list();
         } catch (Exception e) {
             throw new DaoException(e);
         }
